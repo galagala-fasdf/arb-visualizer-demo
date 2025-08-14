67efbbd75a919f446488ed6a46665fd58cda9c2c
galagala-fasdf <hogarpuppyy@gmail.com>
2025-08-14 13:58:52 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,8 @@
-
+fetch("/api/data")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("output").innerHTML =
+      `交易对: ${data.pair} <br>
+       交易所A: ${data.exchangeA} <br>
+       交易所B: ${data.exchangeB}`;
+  });
